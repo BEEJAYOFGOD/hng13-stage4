43e6e5d84efa8ee31748dea4b3e37ebafcca628c
Beejayofgod <adexbolaji100@gmail.com>
2025-11-12 00:58:57 +0100

    fix: userPost index built on firebase

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,13 +1,14 @@
 import { StyleSheet } from "react-native";
 
 import PostComponent from "@/components/PostComponent";
-import { View } from "@/components/Themed";
+// import { Text, View } from "@/components/Themed";
 import EmptyPostState from "@/components/ui/EmptyPostState";
 import ErrorComponent from "@/components/ui/ErrorComponent";
 import LoadingSpinner from "@/components/ui/LoadingSpinner";
 import { useAuth } from "@/context/authcontext";
 import { db } from "@/firebaseconfig";
 import { Post } from "@/types/post";
+import { MaterialIcons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
 import {
     collection,
@@ -17,7 +18,7 @@ import {
     where,
 } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import { FlatList } from "react-native";
+import { FlatList, Text, View } from "react-native";
 
 export default function Profile() {
     const { logOut, user } = useAuth();
@@ -76,39 +77,77 @@ export default function Profile() {
 
     return (
         <View style={styles.container}>
-            <FlatList
-                data={userPosts}
-                renderItem={PostComponent}
-                keyExtractor={(item) => item.id}
-                contentContainerStyle={
-                    userPosts.length === 0
-                        ? styles.emptyList
-                        : styles.listContent
-                }
-                ListEmptyComponent={
-                    <EmptyPostState handleCreatePost={handleCreatePost} />
-                }
-                showsVerticalScrollIndicator={false}
-            />
+            <View style={styles.userInfo}>
+
+                <View style={styles.avatar}>
+                    <Text style={styles.avatarText}>
+                        {user?.displayName[0]}
+                    </Text>
+                </View>
+                <View>
+                    <MaterialIcons name="logout" size={24} color="#2f95dcss" />
+                </View>
+            </View>
+            <Text style={styles.displayName}>@{user?.displayName}</Text>
+            <View style={styles.postContainer}>
+                <FlatList
+                    data={userPosts}
+                    renderItem={PostComponent}
+                    keyExtractor={(item) => item.id}
+                    contentContainerStyle={
+                        userPosts.length === 0
+                            ? styles.emptyList
+                            : styles.listContent
+                    }
+                    ListEmptyComponent={
+                        <EmptyPostState handleCreatePost={handleCreatePost} />
+                    }
+                    showsVerticalScrollIndicator={false}
+                />
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    userInfo:{
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    postContainer: {
+        // width: 100,
+        // borderWidth: 40,
+        // borderColor: "red",
+    },
+    avatar: {
+        width: 80,
+        height: 80,
+        borderRadius: 80,
+        backgroundColor: "#2f95dc",
+        justifyContent: "center",
+        alignItems: "center",
+        marginBottom: 10,
+    },
+    avatarText: {
+        color: "white",
+        fontSize: 30,
+        fontWeight: "bold",
+    },
+    displayName: {
+        marginBottom: 12,
+    },
     container: {
         flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
+        // alignItems: "center",
+        // justifyContent: "center",
+        paddingTop: 40,
+        padding: 20,
     },
     title: {
         fontSize: 20,
         fontWeight: "bold",
         marginBottom: 16,
-    },
-    separator: {
-        marginVertical: 30,
-        height: 1,
-        width: "80%",
     },
 
     centerContainer: {
@@ -121,7 +160,6 @@ const styles = StyleSheet.create({
         flexGrow: 1,
     },
     listContent: {
-        padding: 16,
+        // padding: 16,
     },
 });
-
